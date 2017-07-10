a79226657a030ba4aa5921a1bb86921cad7d8eb7
Roman Malkevych <rmalkevy@e1r4p5.unit.ua>
2017-07-10 18:43:03 +0300

    Add ex02/day04

---
+++ day04/ex02/ISquad.h
@@ -0,0 +1,18 @@
+//
+// Created by Roman Malkevych on 7/10/17.
+//
+
+#ifndef ISQUAD_H
+#define ISQUAD_H
+
+#include <iostream>
+
+class ISquad
+{
+public:
+	virtual ~ISquad() {}
+	virtual int getCount() const = 0;
+	virtual ISpaceMarine* getUnit(int) const = 0;
+	virtual int push(ISpaceMarine*) = 0;
+};
+#endif
